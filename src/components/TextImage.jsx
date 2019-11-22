c3c6fefdefe253b63b3755906d6d8ebf7610f551
Kajsa1 <kajsaKeklof@gmail.com>
2019-11-22 13:24:31 +0000

    Co-authored-by: emmabjork <bjorkemma88@gmail.com> Co-authored-by: annelievmf <annelie.vmf@gmail.com> Co-authored-by: linnearydh <linnea.rydh@live.se>

---
+++ src/components/TextImage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class TextImage extends Component {
+    state = {  }
+    render() { 
+        return ( <div></div>);
+    }
+}
+ 
+export default TextImage;
